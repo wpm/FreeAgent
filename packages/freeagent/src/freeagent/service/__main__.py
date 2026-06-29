bb26dd47379276031e3a84ad751b1007ee003bbf
W.P. McNeill <billmcn@gmail.com>
2026-06-29 17:52:31 -0500

    rebase!: Start Completely Over

--- packages/freeagent/src/freeagent/service/__main__.py
+++
@@ -1,8 +0,0 @@
-"""``python -m freeagent.service``: serve the control service on localhost."""
-
-from __future__ import annotations
-
-from .server import run
-
-if __name__ == "__main__":
-    run()
